188b6cd7579d76e13231c37ee3824b0f88993ac9
Abhijeet Dharmapurikar <adharmap@codeaurora.org>
2019-05-20 16:36:55 -0700

    sched/walt: Improve the scheduler
    This change is for general scheduler improvement.
    
    Change-Id: I5d89acdde73f5379d68ebc8513d0bbeaac128f5d
    Signed-off-by: Abhijeet Dharmapurikar <adharmap@codeaurora.org>
    Signed-off-by: Jonathan Avila <avilaj@codeaurora.org>
    [render: minor fixups]
    Signed-off-by: Zachariah Kennedy <zkennedy87@gmail.com>

--- kernel/sysctl.c
+++ kernel/sysctl.c
@@ -135,6 +135,9 @@ static int one_thousand = 1000;
 #ifdef CONFIG_SCHED_WALT
 static int two_million = 2000000;
 #endif
+#ifdef CONFIG_INCREASE_MAXIMUM_SWAPPINESS
+static int max_swappiness = 200;
+#endif
 #ifdef CONFIG_PRINTK
 static int ten_thousand = 10000;
 #endif
@@ -416,16 +419,6 @@ static struct ctl_table kern_table[] = {
 		.extra1		= &zero,
 		.extra2		= &one_thousand,
 	},
-	{
-		.procname	= "sched_little_cluster_coloc_fmin_khz",
-		.data		= &sysctl_sched_little_cluster_coloc_fmin_khz,
-		.maxlen		= sizeof(unsigned int),
-		.mode		= 0644,
-		.proc_handler	= sched_little_cluster_coloc_fmin_khz_handler,
-		.extra1		= &zero,
-		.extra2		= &two_million,
-	},
-
 	{
 		.procname       = "sched_asym_cap_sibling_freq_match_pct",
 		.data           = &sysctl_sched_asym_cap_sibling_freq_match_pct,
