cdb886b1ed6ca68f44dfda207825a085cb8089e0
karthick111 <karthickrko61@gmail.com>
2020-01-16 06:56:25 +0000

    drivers: base: Import Realme changes
    Change-Id: I1c192778aa0a64ddabf3073ea20f98bf32dfc419

--- include/linux/firmware.h
+++ include/linux/firmware.h
@@ -52,6 +52,13 @@ int request_firmware_into_buf(const struct firmware **firmware_p,
 	const char *name, struct device *device, void *buf, size_t size);
 
 void release_firmware(const struct firmware *fw);
+
+#ifdef VENDOR_EDIT
+//Ping.Zhang@PSW.BSP.Tp, 2019-10-15, Add interface to get proper fw
+int request_firmware_select(const struct firmware **fw, const char *name,
+		     struct device *device);
+#endif/*VENDOR_EDIT*/
+
 #else
 static inline int request_firmware(const struct firmware **fw,
 				   const char *name,
