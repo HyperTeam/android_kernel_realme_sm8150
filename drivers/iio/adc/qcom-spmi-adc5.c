96b09f7070d737b27a6271d8ba422aa75ae05d2c
karthick111 <karthickrko61@gmail.com>
2020-01-18 11:37:34 +0530

    iio: adc5: Import Realme changes
    Change-Id: If85ab79656292d0e228c684cecde6c02b683cf01

--- drivers/iio/adc/qcom-spmi-adc5.c
+++ drivers/iio/adc/qcom-spmi-adc5.c
@@ -735,6 +735,9 @@ static const struct adc_channels adc_chans_pmic5[ADC_MAX_CHANNEL] = {
 					SCALE_HW_CALIB_DEFAULT)
 	[ADC_MID_CHG_DIV6]	= ADC_CHAN_VOLT("chg_mid_chg", 6,
 					SCALE_HW_CALIB_DEFAULT)
+	/* Charger ID detect */
+	[ADC_GPIO3] 		= ADC_CHAN_VOLT("chgID_voltage_adc", 1,
+					SCALE_HW_CALIB_DEFAULT)
 	[ADC_XO_THERM_PU2]	= ADC_CHAN_TEMP("xo_therm", 1,
 					SCALE_HW_CALIB_XOTHERM)
 	[ADC_BAT_THERM_PU2]	= ADC_CHAN_TEMP("bat_therm_pu2", 1,
@@ -748,11 +751,28 @@ static const struct adc_channels adc_chans_pmic5[ADC_MAX_CHANNEL] = {
 	[ADC_AMUX_THM1_PU2]	= ADC_CHAN_TEMP("amux_thm1_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
 	[ADC_AMUX_THM2_PU2]	= ADC_CHAN_TEMP("amux_thm2_pu2", 1,
-					SCALE_HW_CALIB_THERM_100K_PULLUP)
+					SCALE_HW_CALIB_THERM_100K_PULLUP)			
+#ifdef   VENDOR_EDIT /* tongfeng.huang@BSP.CHG.Basic, 2019/01/16,  for pm8150b gpio1 adc  */
+	[ADC_AMUX_THM3_PU2] = ADC_CHAN_VOLT("amux_thm3_pu2", 1,
+					SCALE_HW_CALIB_DEFAULT)
+#else					
 	[ADC_AMUX_THM3_PU2]	= ADC_CHAN_TEMP("amux_thm3_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
+#endif
+#ifdef   VENDOR_EDIT/*for usbtemp*/
+	[ADC_AMUX_THM4_PU2] = ADC_CHAN_VOLT("amux_thm4_pu2", 1,
+					SCALE_HW_CALIB_DEFAULT)
+#else
 	[ADC_AMUX_THM4_PU2]	= ADC_CHAN_TEMP("amux_thm4_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
+#endif
+#ifdef   VENDOR_EDIT /* tongfeng.huang@BSP.CHG.Basic, 2019/01/16,  for pm8150b gpio1 adc  */
+	[ADC_AMUX_THM4_PU2] = ADC_CHAN_VOLT("amux_thm4_pu2", 1,
+					SCALE_HW_CALIB_DEFAULT)
+#else
+    [ADC_AMUX_THM4_PU2] = ADC_CHAN_TEMP("amux_thm4_pu2", 1,
+                    SCALE_HW_CALIB_THERM_100K_PULLUP)
+#endif
 	[ADC_INT_EXT_ISENSE_VBAT_VDATA]	= ADC_CHAN_POWER("int_ext_isense", 1,
 					SCALE_HW_CALIB_CUR)
 	[ADC_EXT_ISENSE_VBAT_VDATA]	= ADC_CHAN_POWER("ext_isense", 1,
@@ -765,12 +785,30 @@ static const struct adc_channels adc_chans_pmic5[ADC_MAX_CHANNEL] = {
 					SCALE_HW_CALIB_PM5_SMB_TEMP)
 	[ADC_GPIO1_PU2]	= ADC_CHAN_TEMP("gpio1_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
+#ifdef   VENDOR_EDIT/*for usbtemp*/
+	[ADC_GPIO2_PU2]	= ADC_CHAN_VOLT("gpio2_pu2", 1,
+					SCALE_HW_CALIB_DEFAULT)
+#else
 	[ADC_GPIO2_PU2]	= ADC_CHAN_TEMP("gpio2_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
+#endif
 	[ADC_GPIO3_PU2]	= ADC_CHAN_TEMP("gpio3_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
 	[ADC_GPIO4_PU2]	= ADC_CHAN_TEMP("gpio4_pu2", 1,
 					SCALE_HW_CALIB_THERM_100K_PULLUP)
+#ifdef   VENDOR_EDIT /* lizhijie@BSP.CHG.Basic, 2019/07/12*/
+	[ADC_GPIO2_PU2]	= ADC_CHAN_VOLT("gpio2_pu2", 1,
+					SCALE_HW_CALIB_DEFAULT)
+#endif
+//#ifdef   VENDOR_EDIT /* tongfeng.huang@BSP.CHG.Basic, 2018/10/23,
+	[ADC_GPIO3] = ADC_CHAN_VOLT("gpio7_v", 1,
+                    SCALE_HW_CALIB_DEFAULT)
+    [ADC_GPIO4_PU2] = ADC_CHAN_VOLT("gpio8_v", 1,
+                    SCALE_HW_CALIB_DEFAULT)
+//#else
+	//[ADC_GPIO4_PU2]	= ADC_CHAN_TEMP("gpio4_pu2", 1,
+	//				SCALE_HW_CALIB_THERM_100K_PULLUP)
+//#endif
 };
 
 static const struct adc_channels adc_chans_rev2[ADC_MAX_CHANNEL] = {
