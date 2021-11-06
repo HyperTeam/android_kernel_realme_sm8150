93567bd4d9dfe8f1113f028ff816a18adeeee13e
Danny Lin <danny@kdrag0n.dev>
2021-11-06 19:54:03 +0800

    Revert "drivers: GICv3: Enable logging of interrupts that triggered wakeup"
    This reverts commit 081c1c9550121d8806e611f002bced1f840536d8.
    
    Redundant:
    
    [  356.028624] GICv3: gic_show_resume_irq: 111 triggered WLAN_CE_2
    [  356.028660] pm_system_irq_wakeup: 111 triggered WLAN_CE_2
    
    Signed-off-by: Danny Lin <danny@kdrag0n.dev>
    Signed-off-by: Cyber Knight <cyberknight755@gmail.com>

--- drivers/irqchip/irq-gic-v3.c
+++ drivers/irqchip/irq-gic-v3.c
@@ -383,38 +383,6 @@ static int gic_irq_set_vcpu_affinity(struct irq_data *d, void *vcpu)
 	return 0;
 }
 
-static void gic_show_resume_irq(struct gic_chip_data *gic)
-{
-	unsigned int i;
-	u32 enabled;
-	u32 pending[32];
-	void __iomem *base = gic_data.dist_base;
-
-	if (base == NULL)
-		return;
-
-	for (i = 0; i * 32 < gic->irq_nr; i++) {
-		enabled = readl_relaxed(base + GICD_ICENABLER + i * 4);
-		pending[i] = readl_relaxed(base + GICD_ISPENDR + i * 4);
-		pending[i] &= enabled;
-	}
-
-	for (i = find_first_bit((unsigned long *)pending, gic->irq_nr);
-	     i < gic->irq_nr;
-	     i = find_next_bit((unsigned long *)pending, gic->irq_nr, i+1)) {
-		unsigned int irq = irq_find_mapping(gic->domain, i);
-		struct irq_desc *desc = irq_to_desc(irq);
-		const char *name = "null";
-
-		if (desc == NULL)
-			name = "stray irq";
-		else if (desc->action && desc->action->name)
-			name = desc->action->name;
-
-		pr_warn("%s: %d triggered %s\n", __func__, irq, name);
-	}
-}
-
 static u64 gic_mpidr_to_affinity(unsigned long mpidr)
 {
 	u64 aff;
